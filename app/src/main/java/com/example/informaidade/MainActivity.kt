5a352e9066acd3d5c87db0566d5585658ac2ea7e
MATHEUS PEREIRA DE AGUIAR <25204557@dsn127m1249389.cfp127aluno.local>
2026-09-22 10:16:43 -0300

    Finalizando bonus de validacao e campo de texto

--- app/src/main/java/com/example/informaidade/MainActivity.kt
+++ app/src/main/java/com/example/informaidade/MainActivity.kt
@@ -57,16 +57,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicsComponentsScreen(modifier: Modifier = Modifier) {
-    var idade by remember {
-        mutableStateOf(17)
-    }
+    var textoIdade by remember { mutableStateOf("17") }
+
+    //Usar essa variavel para validar a idade caso nao tiver nada ou letra retorna 0
+    val idade = textoIdade.toIntOrNull() ?: 0
 
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
@@ -78,7 +78,7 @@ fun BasicsComponentsScreen(modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.SemiBold,
                 color = Color(64, 110, 189, 255)
             )
-        }
+        }//ROW
 
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -86,82 +86,95 @@ fun BasicsComponentsScreen(modifier: Modifier = Modifier) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
-                text = "Aperte os botoes para informar sua idade",
+                text = "Digite ou aperte os botões",
                 fontSize = 16.sp,
                 fontWeight = FontWeight.SemiBold,
                 color = Color(21, 21, 21, 255)
             )
-        }
+        }//ROW
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
-        ){
-            Text(
-                text = idade.toString(),
-                modifier = Modifier
-                    .padding(20.dp),
-                fontSize = 32.sp)
-        }
+        ) {
+            TextField(
+                value = textoIdade,
+                onValueChange = { novoTexto ->
+                    textoIdade = novoTexto
+                },
+                modifier = Modifier.padding(top = 16.dp),
+                label = { Text("Idade") }
+            )
+        }//ROW
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
-        ){
+        ) {
             Button(
                 onClick = {
-                    idade = idade - 1
+                    val atual = textoIdade.toIntOrNull() ?: 0
+                    if (atual > 0) {
+                        textoIdade = (atual - 1).toString()
+                    }
                 },
                 colors = ButtonDefaults.buttonColors(containerColor = Color(64, 110, 189, 255)),
                 modifier = Modifier
-                    .padding(horizontal = 16.dp)
+                    .padding(horizontal = 16.dp, vertical = 16.dp)
                     .size(56.dp),
                 contentPadding = PaddingValues(0.dp),
                 shape = RoundedCornerShape(8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    modifier = Modifier.size(32.dp),
+                    contentDescription = "Diminuir idade"
+                )
+            }//CONFIGURACAO DO BOTAO
 
-            ){
-                Row( verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(imageVector = Icons.Default.KeyboardArrowDown,
-                        modifier = Modifier.size(32.dp),
-                        contentDescription = "Diminuir idade")
-                }//Botao de menos
-            }
             Button(
                 onClick = {
-                    idade = idade + 1
+                    val atual = textoIdade.toIntOrNull() ?: 0
+                    if (atual < 180) {
+                        textoIdade = (atual + 1).toString()
+                    }
                 },
                 colors = ButtonDefaults.buttonColors(containerColor = Color(64, 110, 189, 255)),
                 modifier = Modifier
-                    .padding(horizontal = 16.dp)
+                    .padding(horizontal = 16.dp, vertical = 16.dp)
                     .size(56.dp),
                 contentPadding = PaddingValues(0.dp),
                 shape = RoundedCornerShape(8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    modifier = Modifier.size(32.dp),
+                    contentDescription = "Aumentar idade"
+                )
+            }//CONFIGURACAO DO BOTAO
+        }//BOTAO
 
-            ){
-                Row( verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(imageVector = Icons.Default.KeyboardArrowUp,
-                        modifier = Modifier.size(32.dp),
-                        contentDescription = "Diminuir idade")
-                }//Botao de mais
-            }
-        }
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
-        ){
+        ) {
             Text(
-                text = if (idade <= 17){
-                    "Menor de idade"
-                } else{
-                    "Maior de idade"
-                },
+                text = if (idade < 0) {
+                      "Número não é permitido"}
+                        else if(idade > 180) {
+                            "Número não permitido"}
+                        else if(idade <= 17){
+                            "Você é MENOR de idade"}
+                else {  "Você é MAIOR de idade"},
                 color = Color(64, 110, 189, 255),
                 modifier = Modifier.padding(top = 16.dp),
-                fontSize = 32.sp)
-        }
-    }
-}
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }//ROW
+    }//COLUMN
+}//FUNCAO
 
